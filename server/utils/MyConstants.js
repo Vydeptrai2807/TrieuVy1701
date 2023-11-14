4dbe62d5de936c870801118e79e86d253d8a7e01
VyTran2807 <128352682+Vydeptrai2807@users.noreply.github.com>
2023-11-14 13:53:54 +0700

    init

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.jbsyygg.mongodb.net',
+    DB_USER: 'trieuvy',
+    DB_PASS: 'trieuvy123456',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'abc',
+    JWT_EXPIRES: '36000000', // in milliseconds
+    EMAIL_USER: 'vyx287901@gmail.com', // gmail service
+    EMAIL_PASS: 'vxkx iahf vuyj ivln'
+  };
+  module.exports = MyConstants;
